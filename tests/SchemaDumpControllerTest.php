96eb66ef4e37a6b4adca867b2c77f5d916239591
Tomoki Morita <tmsongbooks215@gmail.com>
2017-01-22 17:30:28 +0900

    Add some test case and several improvements

--- tests/SchemaDumpControllerTest.php
+++ tests/SchemaDumpControllerTest.php
@@ -45,6 +45,7 @@ class SchemaDumpControllerText extends \PHPUnit_Framework_TestCase
     {
         $db = Yii::$app->db;
         foreach ($db->schema->getTableNames() as $table) {
+            $db->createCommand()->checkIntegrity(false)->execute();
             $db->createCommand("DROP TABLE `$table`")->execute();
         }
     }
@@ -53,26 +54,109 @@ class SchemaDumpControllerText extends \PHPUnit_Framework_TestCase
     {
         $this->controller->run('create');
         $this->assertSame(<<<'STDOUT'
-// post
-$this->createTable('{{%post}}', [
+// 0010_pk_ai
+$this->createTable('{{%0010_pk_ai}}', [
     'id' => Schema::TYPE_PK,
-    'user_id' => Schema::TYPE_INTEGER."(11) NOT NULL",
-    'title' => Schema::TYPE_STRING."(255) NOT NULL",
-    'body' => Schema::TYPE_TEXT." NOT NULL",
-    'created_at' => Schema::TYPE_INTEGER."(11) NOT NULL",
-    'updated_at' => Schema::TYPE_INTEGER."(11) NOT NULL",
 ], $this->tableOptions);
 
-// user
-$this->createTable('{{%user}}', [
-    'id' => Schema::TYPE_PK." COMMENT '主キー'",
-    'username' => Schema::TYPE_STRING."(255) NOT NULL COMMENT 'ユーザ名'",
-    'password' => Schema::TYPE_STRING."(255) NOT NULL COMMENT 'パスワード'",
+// 0020_pk_not_ai
+$this->createTable('{{%0020_pk_not_ai}}', [
+    'id' => Schema::TYPE_INTEGER."(11) NOT NULL",
+    'PRIMARY KEY (id)',
 ], $this->tableOptions);
 
-// fk: post
-$this->addForeignKey('fk_post_user_id', '{{%post}}', 'user_id', '{{%user}}', 'id');
+// 0030_pk_bigint_ai
+$this->createTable('{{%0030_pk_bigint_ai}}', [
+    'id' => Schema::TYPE_BIGPK,
+], $this->tableOptions);
 
+// 0040_pk_unsigned_ai
+$this->createTable('{{%0040_pk_unsigned_ai}}', [
+    'id' => Schema::TYPE_INTEGER."(10) UNSIGNED NOT NULL AUTO_INCREMENT",
+    'PRIMARY KEY (id)',
+], $this->tableOptions);
+
+// 0050_pk_bigint_unsigned_ai
+$this->createTable('{{%0050_pk_bigint_unsigned_ai}}', [
+    'id' => Schema::TYPE_BIGINT."(20) UNSIGNED NOT NULL AUTO_INCREMENT",
+    'PRIMARY KEY (id)',
+], $this->tableOptions);
+
+// 0060_composite_pks
+$this->createTable('{{%0060_composite_pks}}', [
+    'foo_id' => Schema::TYPE_INTEGER."(11) NOT NULL",
+    'bar_id' => Schema::TYPE_INTEGER."(11) NOT NULL",
+    'PRIMARY KEY (foo_id, bar_id)',
+], $this->tableOptions);
+
+// 0100_types
+$this->createTable('{{%0100_types}}', [
+    'char' => Schema::TYPE_CHAR."(20) NOT NULL",
+    'varchar' => Schema::TYPE_STRING."(20) NOT NULL",
+    'text' => Schema::TYPE_TEXT." NOT NULL",
+    'smallint' => Schema::TYPE_SMALLINT."(6) NOT NULL",
+    'integer' => Schema::TYPE_INTEGER."(11) NOT NULL",
+    'bigint' => Schema::TYPE_BIGINT."(20) NOT NULL",
+    'float' => Schema::TYPE_FLOAT." NOT NULL",
+    'float_decimal' => Schema::TYPE_FLOAT."(20,10) NOT NULL",
+    'double' => Schema::TYPE_DOUBLE."(20,10) NOT NULL",
+    'decimal' => Schema::TYPE_DECIMAL."(20,10) NOT NULL",
+    'money' => Schema::TYPE_DECIMAL."(19,4) NOT NULL",
+    'datetime' => Schema::TYPE_DATETIME." NOT NULL",
+    'timestamp' => Schema::TYPE_TIMESTAMP." NOT NULL DEFAULT CURRENT_TIMESTAMP",
+    'time' => Schema::TYPE_TIME." NOT NULL",
+    'date' => Schema::TYPE_DATE." NOT NULL",
+    'binary' => Schema::TYPE_BINARY." NOT NULL",
+    'boolean' => Schema::TYPE_BOOLEAN." NOT NULL DEFAULT '0'",
+    'tinyint_1' => Schema::TYPE_BOOLEAN." NOT NULL DEFAULT '0'",
+], $this->tableOptions);
+
+// 0200_default_values
+$this->createTable('{{%0200_default_values}}', [
+    'string' => Schema::TYPE_STRING."(255) NOT NULL DEFAULT 'UNKNOWN'",
+    'special_characters' => Schema::TYPE_STRING."(255) NOT NULL DEFAULT '\'\"'",
+], $this->tableOptions);
+
+// 0300_comment
+$this->createTable('{{%0300_comment}}', [
+    'username' => Schema::TYPE_STRING."(20) NOT NULL COMMENT 'ユーザ名'",
+    'special_characters' => Schema::TYPE_STRING."(20) NOT NULL COMMENT '\'\"'",
+], $this->tableOptions);
+
+// 0400_fk_parent
+$this->createTable('{{%0400_fk_parent}}', [
+    'id' => Schema::TYPE_PK,
+], $this->tableOptions);
+
+// 0410_fk_child
+$this->createTable('{{%0410_fk_child}}', [
+    'id' => Schema::TYPE_PK,
+    'parent_id' => Schema::TYPE_INTEGER."(11) NOT NULL",
+], $this->tableOptions);
+
+// fk: 0410_fk_child
+$this->addForeignKey('fk_0410_fk_child_parent_id', '{{%0410_fk_child}}', 'parent_id', '{{%0400_fk_parent}}', 'id');
+
+
+STDOUT
+        , $this->controller->flushStdOutBuffer());
+    }
+
+    public function testDropAction()
+    {
+        $this->controller->run('drop');
+        $this->assertSame(<<<'STDOUT'
+$this->dropTable('{{%0010_pk_ai}}');
+$this->dropTable('{{%0020_pk_not_ai}}');
+$this->dropTable('{{%0030_pk_bigint_ai}}');
+$this->dropTable('{{%0040_pk_unsigned_ai}}');
+$this->dropTable('{{%0050_pk_bigint_unsigned_ai}}');
+$this->dropTable('{{%0060_composite_pks}}');
+$this->dropTable('{{%0100_types}}');
+$this->dropTable('{{%0200_default_values}}');
+$this->dropTable('{{%0300_comment}}');
+$this->dropTable('{{%0400_fk_parent}}');
+$this->dropTable('{{%0410_fk_child}}'); // fk: parent_id
 
 STDOUT
         , $this->controller->flushStdOutBuffer());
--- tests/mysql.sql
+++ tests/mysql.sql
@@ -1,19 +1,83 @@
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '主キー',
-    `username` VARCHAR(255) NOT NULL COMMENT 'ユーザ名',
-    `password` VARCHAR(255) NOT NULL COMMENT 'パスワード',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB CHARACTER SET=utf8 COLLATE=utf8_unicode_ci;
-
-DROP TABLE IF EXISTS `post`;
-CREATE TABLE `post` (
+-- for some primary keys
+DROP TABLE IF EXISTS `0010_pk_ai`;
+CREATE TABLE `0010_pk_ai` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `user_id` INT(11) NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `body` TEXT NOT NULL,
-    `created_at` INT(11) NOT NULL,
-    `updated_at` INT(11) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `0020_pk_not_ai`;
+CREATE TABLE `0020_pk_not_ai` (
+    `id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `0030_pk_bigint_ai`;
+CREATE TABLE `0030_pk_bigint_ai` (
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `0040_pk_unsigned_ai`;
+CREATE TABLE `0040_pk_unsigned_ai` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `0050_pk_bigint_unsigned_ai`;
+CREATE TABLE `0050_pk_bigint_unsigned_ai` (
+    `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `0060_composite_pks`;
+CREATE TABLE `0060_composite_pks` (
+    `foo_id` INT(11) NOT NULL,
+    `bar_id` INT(11) NOT NULL,
+    PRIMARY KEY (`foo_id`, `bar_id`)
+);
+
+-- for some types
+DROP TABLE IF EXISTS `0100_types`;
+CREATE TABLE `0100_types` (
+    `char` CHAR(20) NOT NULL,
+    `varchar` VARCHAR(20) NOT NULL,
+    `text` TEXT NOT NULL,
+    `smallint` SMALLINT(6) NOT NULL,
+    `integer` INT(11) NOT NULL,
+    `bigint` BIGINT(20) NOT NULL,
+    `float` FLOAT NOT NULL,
+    `float_decimal` FLOAT(20,10) NOT NULL,
+    `double` DOUBLE(20,10) NOT NULL,
+    `decimal` DECIMAL(20,10) NOT NULL,
+    `money` DECIMAL(19,4) NOT NULL,
+    `datetime` DATETIME NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL,
+    `time` TIME NOT NULL,
+    `date` DATE NOT NULL,
+    `binary` BLOB NOT NULL,
+    `boolean` BOOLEAN NOT NULL DEFAULT 0,
+    `tinyint_1` TINYINT(1) NOT NULL DEFAULT 0
+);
+
+-- for some default values
+DROP TABLE IF EXISTS `0200_default_values`;
+CREATE TABLE `0200_default_values` (
+    `string` VARCHAR(255) NOT NULL DEFAULT 'UNKNOWN',
+    `special_characters` VARCHAR(255) NOT NULL DEFAULT '\'"'
+);
+
+-- for some comments
+DROP TABLE IF EXISTS `0300_comment`;
+CREATE TABLE `0300_comment` (
+    `username` VARCHAR(20) NOT NULL COMMENT 'ユーザ名',
+    `special_characters` VARCHAR(20) NOT NULL COMMENT '\'"'
+);
+
+-- for foreign keys
+DROP TABLE IF EXISTS `0400_fk_parent`;
+CREATE TABLE `0400_fk_parent` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `0410_fk_child`;
+CREATE TABLE `0410_fk_child` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `parent_id` INT(11) NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB CHARACTER SET=utf8 COLLATE=utf8_unicode_ci;
+    FOREIGN KEY (`parent_id`) REFERENCES `0400_fk_parent` (`id`)
+);
